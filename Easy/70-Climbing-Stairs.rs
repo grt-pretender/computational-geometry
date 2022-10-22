fc8524694af90e93cb9bded2559abb43190eef77
Daria Mashkova <dmetafrasis@gmail.com>
2022-10-23 02:38:18 +0700

    Add file

---
+++ Easy/70-Climbing-Stairs.rs
@@ -0,0 +1,26 @@
+// You are climbing a staircase.
+// It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps.
+// In how many distinct ways can you climb to the top?
+
+// ==========================
+
+// using dynamic programming, memorization, bottom-up approach
+// using two pointers, each step capacity is the sum of the two previous ones,
+// like Fibonacci numbers
+
+impl Solution {
+    pub fn climb_stairs(n: i32) -> i32 {
+        let mut first_pointer: i32 = 1;
+        let mut second_pointer: i32 = 1;
+
+        for i in 0..n - 1 {
+            let mut temporary = first_pointer;
+            first_pointer = first_pointer + second_pointer;
+            second_pointer = temporary;
+        }
+
+        return first_pointer;
+    }
+}
