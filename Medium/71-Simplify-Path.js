74232e0d2dc1cd5093993bed479e36fe8006c3ff
Daria Mashkova <dmetafrasis@gmail.com>
2023-11-16 01:03:00 +0700

    Add file

---
+++ Medium/71-Simplify-Path.js
@@ -0,0 +1,39 @@
+// Given a string path, which is an absolute path (starting with a slash '/') 
+// to a file or directory in a Unix-style file system, 
+// convert it to the simplified canonical path.
+
+// In a Unix-style file system, a period '.' refers to the current directory, 
+// a double period '..' refers to the directory up a level, 
+// and any multiple consecutive slashes (i.e. '//') are treated as a single slash '/'. 
+// For this problem, any other format of periods such as '...' are treated as file/directory names.
+
+// The canonical path should have the following format:
+
+// The path starts with a single slash '/'.
+// Any two directories are separated by a single slash '/'.
+// The path does not end with a trailing '/'.
+// The path only contains the directories on the path from the root directory 
+// to the target file or directory (i.e., no period '.' or double period '..')
+// Return the simplified canonical path.
+
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function(path) {
+    var ourStack = [];
+    var pathDivided = path.split('/');
+  
+    for (var i = 0; i < pathDivided.length; i++) {
+        element = pathDivided[i];
+        if (element === '' || element === '.') continue;
+        if (element === '..') {
+        ourStack.pop();
+        } else {
+        ourStack.push(element);
+        }
+    }
+    return '/' + ourStack.join('/');
+    
+};
+
